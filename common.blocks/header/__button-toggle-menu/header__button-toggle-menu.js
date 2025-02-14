81b0ded78b824f7bd89b12f47af76ff31b11b7e1
Oksana Turova <psdhtmlcss@mail.ru>
2025-02-14 11:51:40 +0400

    markup edits

--- common.blocks/header/__button-toggle-menu/header__button-toggle-menu.js
+++ common.blocks/header/__button-toggle-menu/header__button-toggle-menu.js
@@ -1,10 +1,12 @@
 const buttonToggleMenu = document.querySelector('.header__button-toggle-menu');
 const headerNav = document.querySelector('.header__nav');
+const pageBody = document.querySelector('.page__body');
 
 const onButtonToggleMenu = (evt) => {
   evt.preventDefault;
   evt.currentTarget.classList.toggle('header__button-toggle-menu_active');
   headerNav.classList.toggle('header__nav_show');
+  pageBody.classList.toggle('page__body_overflow_hidden');
 }
 
 buttonToggleMenu.addEventListener('click', onButtonToggleMenu);
